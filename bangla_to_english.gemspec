3003c81ed0bc5e50a4c66f3cfc302cab66b127df
MD. Tawab Alam Khan <cs.antorkhan@gmail.com>
2020-08-12 15:57:02 +0600

    Changed to Class Based code with validation

--- bangla_to_english.gemspec
+++ bangla_to_english.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
 	s.name        = 'BanglaToEnglish'
-	s.version     = '0.0.0'
-	s.date        = '2020-08-11'
+	s.version     = '0.1.0'
+	s.date        = '2020-08-12'
 	s.summary     = "Convert Bangla Text to English"
 	s.description = "A simple gem to convert Bangla text to English based on phonetics matching"
 	s.authors     = ["MD Tawab Alam Khan"]
--- lib/bangla_to_english.rb
+++ lib/bangla_to_english.rb
@@ -1,8 +1,24 @@
 require('./lib//patterns')
+class BanglaToEnglish
+	attr_accessor :bangla_text
 
-def self.convert_to_english( bangla_text )
-	english_text = ''
-	bangla_text&.chars.map { |bangla_font| english_text += @patterns[bangla_font]||'' }
-	english_text
+	def initialize(bangla_text)
+		@bangla_text = bangla_text
+  end
+
+	def convert_to_english
+		english_text = ''
+		@bangla_text&.chars.map do |font|
+      english_text += bangla_font?(font) ? english_font(font) : font
+    end
+		english_text
+	end
+
+	private
+	def bangla_font? ( font )
+    font.ord >= 2432 && font.ord <= 2543
+	end
+
 end
+
 
