a74d27299bde16be98dd821fafe87b1882f2d207
MD. Tawab Alam Khan <cs.antorkhan@gmail.com>
2020-08-13 12:34:37 +0600

    Fixed File Dependency Issue

--- bangla_to_english.gemspec
+++ bangla_to_english.gemspec
@@ -7,6 +7,7 @@ Gem::Specification.new do |s|
 	s.authors     = ["MD Tawab Alam Khan"]
 	s.email       = 'cs.antorkhan@gmail.com'
 	s.files       = ["lib/bangla_to_english.rb"]
+	s.files       += Dir['lib/*.rb']
 	s.homepage    =
 		'https://github.com/antorkhan/bangla-to-english'
 	s.license       = 'MIT'
--- lib/bangla_to_english.rb
+++ lib/bangla_to_english.rb
@@ -1,22 +1,22 @@
-require('./lib//patterns')
+require_relative ('patterns')
 class BanglaToEnglish
 	attr_accessor :bangla_text
 
 	def initialize(bangla_text)
 		@bangla_text = bangla_text
-  end
+	end
 
 	def convert_to_english
 		english_text = ''
 		@bangla_text&.chars.map do |font|
-      english_text += bangla_font?(font) ? english_font(font) : font
-    end
+			english_text += bangla_font?(font) ? english_font(font) : font
+		end
 		english_text
 	end
 
 	private
 	def bangla_font? ( font )
-    font.ord >= 2432 && font.ord <= 2543
+		font.ord >= 2432 && font.ord <= 2543
 	end
 
 end
